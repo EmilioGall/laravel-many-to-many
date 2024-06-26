b75f2428073b5e6923286e4e86ce8813f4da6950
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-26 17:57:57 +0200

    feat: added validation to Request - UpdateTypeRequest.php

--- app/Http/Requests/UpdateTypeRequest.php
+++ app/Http/Requests/UpdateTypeRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateTypeRequest extends FormRequest
+class UpdateTypeRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,6 +24,7 @@ class CreateTypeRequest extends FormRequest
         return [
 
             'name' => ['unique:types,name', 'min:3']
+
         ];
     }
 
