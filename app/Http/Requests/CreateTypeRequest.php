8db07050c82095acba581d31e85cb0c23f44e185
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-26 17:24:11 +0200

    feat: added validation to Request - CreateTypeRequest.php

---
+++ app/Http/Requests/CreateTypeRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTypeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            
+            'name' => ['unique:types,name']
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+
+            'name.unique' => 'The selected name is already in the list of types.',
+        ];
+    }
+}
