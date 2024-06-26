c465d45a89b3290c4d5f415ee010f15d80b25a51
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-26 15:02:39 +0200

    feat: passed collection of technologies to view through create() method in Controller - ProjectController.php

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -35,7 +36,11 @@ class ProjectController extends Controller
 
         // dd($typesCollection);
 
-        return view('admin.projects.create', compact('typesCollection'));
+        $technologiesCollection = Technology::all();
+
+        // dd($technologiesCollection);
+
+        return view('admin.projects.create', compact('typesCollection', 'technologiesCollection'));
     }
 
     /**
